0047aa4a07fb14358b7ec8ff2aa55ceb6c859728
Alesio Torres <alesiosilva@gmail.com>
2022-12-17 12:31:48 -0300

    Lógica para download de playlist de acordo com URL informada

--- downloader.py
+++ downloader.py
@@ -1,11 +1,19 @@
 from pytube import YouTube
 
-video = YouTube(input('Informe a URL do vídeo ou playlist: \n'))
+url = input('Informe a URL do vídeo ou playlist: \n')
 
-print('Iniciando o download do vídeo ', video.title, ...)
+def validatePlaylist(url):
+    return url.__contains__('/playlist?list=')
 
-stream = video.streams.get_by_itag(22)
+youtube = YouTube(url)
 
-stream.download()
+if (validatePlaylist(url)==False):
+    print('Iniciando o download do vídeo: ', youtube.title, ...)
+    stream = youtube.streams.get_by_itag(22)
+    stream.download()
+else:
+    print('Iniciando o download da Playlist: ', youtube.title, ...)
+    for video in youtube.videos:
+        video.streams.first().download()
 
-print('Download concluído! \n\n\n')
+print('Download concluído!!!')
