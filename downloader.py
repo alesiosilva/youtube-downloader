80689450b21318fd008893cce870f7d7a55239e8
Alesio Torres <alesiosilva@gmail.com>
2022-12-17 21:57:43 -0300

    Ajuste de tratamento de exceções

--- downloader.py
+++ downloader.py
@@ -5,23 +5,24 @@ url = input('Informe a URL do vídeo ou playlist: \n')
 def validatePlaylist(url):
     return url.__contains__('/playlist?list=')
 
-try:
-    if (validatePlaylist(url)==False):
+if (validatePlaylist(url)==False):
+    try:
         video = YouTube(url)
+    except Exception as e:
+        print('Erro: ', e)
     else:
-        playlist = Playlist(url)
-
-except:
-    print('Erro: URL incompatível ou bloqueada para download!')
-
-else:
-    if (validatePlaylist(url)==False):
         print('Iniciando o download do vídeo: ', video.title, ...)
         stream = video.streams.get_by_itag(22)
         stream.download()
+        print('Download concluído!!!')
+
+else:
+    try:
+        playlist = Playlist(url)
+    except Exception as e:
+        print('Erro: ', e)
     else:
         print('Iniciando o download da Playlist: ', playlist.title, ...)
         for video in playlist.videos:
             video.streams.first().download()
-
-print('Download concluído!!!')
+        print('Downloads concluídos!!!')
